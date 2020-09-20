b6b71091dbd07751bf531ad4f49abfc8b5a478c8
Yurii Smal <smalygga1488@gmail.comgit config --global user.name Yurii>
2020-09-20 13:05:56 +0300

    homework done

---
+++ main.js
@@ -0,0 +1,63 @@
+// 1. Write a JavaScript program which computes 
+// the average marks of the following students. Then, this average is used to determine the corresponding grade.
+
+// let marks = [88, 77, 68, 88, 95];
+// let grade = ['F', 'D', 'C', 'B', 'A'];
+// let sum = 0;
+// let avg;
+
+// for (let i = 0; i < marks.length; i++) {
+//     sum += marks[i];
+//     avg = sum / marks.length;
+// }
+
+// alert(`The average mark is ${avg}.`);
+
+// switch (true) {
+//     case avg < 60:
+//         alert(`The corresponding grade = ${grade[0]}`);
+//         break;
+//     case avg  < 70:
+//         alert(`The corresponding grade = ${grade[1]}`);
+//         break;
+//     case avg < 80:
+//         alert(`The corresponding grade = ${grade[2]}`);
+//         break;
+//     case avg < 90:
+//         alert(`The corresponding grade = ${grade[3]}`);
+//         break;
+//     case avg < 100:
+//         alert(`The corresponding grade = ${grade[4]}`);
+//         break;
+//     default:
+//         alert("Error");
+// }
+
+// 2. Write a JavaScript program to sum the multiples of 3 or 5 under 1000.
+
+// var sum = 0;
+
+// for (let i = 3; i <1000; i++) {
+//      if (i % 3 == 0 || i % 5 == 0) {
+//          sum += i;
+//      }
+// }
+
+// alert(`Sum = ${sum}.`);
+
+// 3. Write a JavaScript program to construct the following pattern, using a nested for loop.
+
+// let star = '*';
+// let space = ' ';
+
+// for (let i = 0; i < 1; i++) {
+//         star = star + space;
+    
+//     for (let j = 0; j < 5; j++) {
+//         console.log(star); 
+//         star += star[i] + space;
+       
+//     }
+// }
+
+
